3ef4cf7b999b6561409f515a74d2fff7bcf39294
aleng1106 <ltlobygg@163.com>
2023-11-13 11:05:49 +0800

    remove

--- src/graphql/tempCodeRunnerFile.ts
+++
@@ -1,7 +0,0 @@
-ery: {
-    //   getHero: (obj: any, context: any, info: any) => {
-    //     console.log(context);
-
-    //     return { __typename: "Human", name: "R2-D2", height: "height" };
-    //   },
-    // },
